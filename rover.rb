be02e4c4b8aa77a41250a2259fcab018ec7b70dc
Ernest S <ernest.s@multunus.com>
2013-05-02 00:26:29 +0530

    refactored a little

--- rover.rb
+++ rover.rb
@@ -15,29 +15,44 @@ class Rover
     :E_M => "E"
   }
   def initialize max_size_of_plateau, initial_position_of_rover
-    @max_size_of_plateau = max_size_of_plateau
-    @initial_position_of_rover = initial_position_of_rover
-    @current_position_of_rover = @initial_position_of_rover
+    self.current_position_of_rover = initial_position_of_rover
   end
   
   def update_position instruction
+    self.update_direction instruction
+    if instruction == 'M'
+      self.move_rover instruction
+    end
+  end
+  def move_rover_towards_north
+    self.current_position_of_rover[:y] = self.current_position_of_rover[:y] + 1
+  end
+  def move_rover_towards_south
+    self.current_position_of_rover[:y] = self.current_position_of_rover[:y] - 1
+  end
+  def move_rover_towards_east
+    self.current_position_of_rover[:x] = self.current_position_of_rover[:x] + 1
+  end
+  def move_rover_towards_west
+    self.current_position_of_rover[:x] = self.current_position_of_rover[:x] - 1
+  end
 
+
+  def move_rover instruction
+    moveHash = {
+      "N" => "north",
+      "S" => "south",
+      "W" => "west",
+      "E" => "east"
+    }
+    self.send("move_rover_towards_#{moveHash[self.current_position_of_rover[:direction]]}")
+  end
+
+  def update_direction instruction
     direction = @@directionHash["#{self.current_position_of_rover[:direction]}_#{instruction}".to_sym]
     self.current_position_of_rover[:direction] = direction
-
-    if instruction == 'M' && self.current_position_of_rover[:direction] == "N"
-      self.current_position_of_rover[:y] = self.current_position_of_rover[:y] + 1
-    end
-    if instruction == 'M' && self.current_position_of_rover[:direction] == "S"
-      self.current_position_of_rover[:y] = self.current_position_of_rover[:y] - 1
-    end
-    if instruction == 'M' && self.current_position_of_rover[:direction] == "W"
-      self.current_position_of_rover[:x] = self.current_position_of_rover[:x] - 1
-    end
-    if instruction == 'M' && self.current_position_of_rover[:direction] == "E"
-      self.current_position_of_rover[:x] = self.current_position_of_rover[:x] + 1
-    end    
   end
+
   def calculate_final_position(instruction_set)
     instruction_set = instruction_set.split(//)
     instruction_set.each do  |instruction|
